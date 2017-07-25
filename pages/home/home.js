12ce251c6a3147e601283b62875c1dc40665589c
ddddd <1040589344@qq.com>
2017-07-25 18:10:23 +0800

    添加广告栏

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,7 +4,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isShowText: true
+    isShowText: true,
+    imgUrls:[
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
 
   /**
