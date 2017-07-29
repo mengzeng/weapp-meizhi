0deed3b8981847d4d5f8bee6d61b2e5b781a5ef8
dddd <1040589344@qq.com>
2017-07-29 23:17:57 +0800

    调通热门电影接口 ，添加首页热映跳转

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     isShowText: true,
-    imgUrls:[
+    imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
@@ -26,64 +26,69 @@ Page({
         userInfo: userInfo
       })
     })
-     wx.request({
-       method:'GET',
-       url: 'https://api.douban.com/v2/book/1220562',
-       header: {
-         'content-type': 'json'
-       },
-       success: function (res) {
-         console.log(res.data)
-       }
-     })
+    wx.request({
+      method: 'GET',
+      url: 'https://api.douban.com/v2/book/1220562',
+      header: {
+        'content-type': 'json'
+      },
+      success: function (res) {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
+  },
+  movieHot: function (event) {
+    wx.navigateTo({
+      url: '/pages/home/movie/movie',
+    })
   }
 })
